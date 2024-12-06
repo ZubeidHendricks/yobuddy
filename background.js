2dd4568f94ce8bb26fdc8f8391f15f5729629c9f
Zubeid Hendricks <zubeid.hendricks@gmail.com>
2024-12-07 01:46:46 +0200

    Update background script to handle voice and cursor features

--- background.js
+++ background.js
@@ -7,49 +7,98 @@ chrome.runtime.onConnect.addListener((port) => {
 
   if (!connections[roomId]) {
     connections[roomId] = [];
-    sessions[roomId] = { currentUrl: null };
+    sessions[roomId] = { 
+      currentUrl: null,
+      users: new Set(),
+      cart: []
+    };
   }
 
-  // Send current URL to new joiner
-  if (sessions[roomId].currentUrl) {
-    port.postMessage({
-      type: 'sync',
-      url: sessions[roomId].currentUrl
-    });
-  }
+  // Generate unique user ID
+  const userId = Math.random().toString(36).substring(7);
+  sessions[roomId].users.add(userId);
 
-  connections[roomId].push(port);
+  // Send current session state to new user
+  port.postMessage({
+    type: 'sessionState',
+    data: {
+      url: sessions[roomId].currentUrl,
+      users: Array.from(sessions[roomId].users),
+      cart: sessions[roomId].cart
+    },
+    userId: userId
+  });
+
+  connections[roomId].push({ port, userId });
+
+  // Broadcast new user joined
+  broadcastToRoom(roomId, {
+    type: 'userJoined',
+    userId: userId
+  }, port);
 
   port.onMessage.addListener((msg) => {
     console.log('Message received:', msg);
-    if (msg.type === 'navigation') {
-      sessions[roomId].currentUrl = msg.url;
-      // Broadcast URL to all other users in room
-      connections[roomId].forEach(p => {
-        if (p !== port) {
-          console.log('Broadcasting URL:', msg.url);
-          p.postMessage({
-            type: 'sync',
-            url: msg.url
-          });
-        }
-      });
-    } else if (msg.type === 'getCurrentUrl') {
-      if (sessions[roomId].currentUrl) {
-        port.postMessage({
+    
+    switch(msg.type) {
+      case 'navigation':
+        sessions[roomId].currentUrl = msg.url;
+        broadcastToRoom(roomId, {
           type: 'sync',
-          url: sessions[roomId].currentUrl
-        });
-      }
+          url: msg.url
+        }, port);
+        break;
+
+      case 'signaling':
+        broadcastToRoom(roomId, {
+          type: 'signaling',
+          data: msg.data,
+          fromUserId: userId
+        }, port);
+        break;
+
+      case 'cursor':
+        broadcastToRoom(roomId, {
+          type: 'cursor',
+          position: msg.position,
+          userId: userId
+        }, port);
+        break;
+
+      case 'cart':
+        sessions[roomId].cart = msg.cart;
+        broadcastToRoom(roomId, {
+          type: 'cartUpdate',
+          cart: msg.cart
+        }, port);
+        break;
     }
   });
 
   port.onDisconnect.addListener(() => {
-    console.log('Connection closed:', roomId);
-    connections[roomId] = connections[roomId].filter(p => p !== port);
+    console.log('User disconnected:', userId);
+    sessions[roomId].users.delete(userId);
+    connections[roomId] = connections[roomId].filter(c => c.port !== port);
+
+    // Broadcast user left
+    broadcastToRoom(roomId, {
+      type: 'userLeft',
+      userId: userId
+    });
+
     if (connections[roomId].length === 0) {
       delete connections[roomId];
       delete sessions[roomId];
     }
   });
 });
+
+function broadcastToRoom(roomId, message, excludePort = null) {
+  if (connections[roomId]) {
+    connections[roomId].forEach(({port}) => {
+      if (port !== excludePort) {
+        port.postMessage(message);
+      }
+    });
+  }
+}
