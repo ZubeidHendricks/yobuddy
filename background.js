56571442541093ce794f29b4aa86d6987fe79d04
Zubeid Hendricks <zubeid.hendricks@gmail.com>
2024-12-07 08:20:25 +0200

    Update background.js with user tracking

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 let connections = {};
+let roomUsers = {};
 
 chrome.runtime.onConnect.addListener((port) => {
   const roomId = port.name;
@@ -6,34 +7,72 @@ chrome.runtime.onConnect.addListener((port) => {
 
   if (!connections[roomId]) {
     connections[roomId] = [];
+    roomUsers[roomId] = new Set();
   }
   connections[roomId].push(port);
 
-  // Handle messages
   port.onMessage.addListener((msg) => {
     console.log('Message received:', msg);
-    // Broadcast message to all other users in room
-    connections[roomId].forEach(p => {
-      if (p !== port) {
-        p.postMessage(msg);
-      }
-    });
+    switch(msg.type) {
+      case 'userJoined':
+        roomUsers[roomId].add(msg.username);
+        broadcastUserList(roomId);
+        broadcastToRoom(roomId, {
+          type: 'userJoined',
+          username: msg.username
+        }, port);
+        break;
+
+      case 'userLeft':
+        roomUsers[roomId].delete(msg.username);
+        broadcastUserList(roomId);
+        broadcastToRoom(roomId, {
+          type: 'userLeft',
+          username: msg.username
+        }, port);
+        break;
+
+      default:
+        broadcastToRoom(roomId, msg, port);
+    }
   });
 
-  // Handle disconnection
   port.onDisconnect.addListener(() => {
-    console.log('Connection closed:', roomId);
+    console.log('Connection closed:', port.name);
     connections[roomId] = connections[roomId].filter(p => p !== port);
+    
     if (connections[roomId].length === 0) {
       delete connections[roomId];
+      delete roomUsers[roomId];
+    } else {
+      broadcastUserList(roomId);
     }
   });
 });
 
-// Log active connections
+function broadcastToRoom(roomId, message, excludePort = null) {
+  if (connections[roomId]) {
+    connections[roomId].forEach(p => {
+      if (p !== excludePort) {
+        p.postMessage(message);
+      }
+    });
+  }
+}
+
+function broadcastUserList(roomId) {
+  if (connections[roomId]) {
+    const users = Array.from(roomUsers[roomId]);
+    broadcastToRoom(roomId, {
+      type: 'userList',
+      users: users
+    });
+  }
+}
+
 setInterval(() => {
-  console.log('Active connections:', Object.keys(connections).map(roomId => ({
+  console.log('Active rooms:', Object.keys(connections).map(roomId => ({
     roomId,
-    users: connections[roomId].length
+    users: Array.from(roomUsers[roomId])
   })));
 }, 5000);
