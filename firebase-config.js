4c647dc4cebd0a7c6fbd03537d1b1b6262b7f733
Zubeid Hendricks <zubeid.hendricks@gmail.com>
2024-12-11 17:00:56 +0200

    Remove Firebase Analytics and update configuration

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-database.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDq0BESYmeLklyKaNgA7KlGGR4TyHinHuo",
@@ -10,15 +9,13 @@ const firebaseConfig = {
   projectId: "yobuddy-89a58",
   storageBucket: "yobuddy-89a58.firebasestorage.app",
   messagingSenderId: "873396866801",
-  appId: "1:873396866801:web:b84e17c8a6514496c3a350",
-  measurementId: "G-87LCDY7YN6"
+  appId: "1:873396866801:web:b84e17c8a6514496c3a350"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const database = getDatabase(app);
-const analytics = getAnalytics(app);
 const provider = new GoogleAuthProvider();
 
-export { app, auth, database, analytics, provider };
+export { app, auth, database, provider };
--- popup.js
+++ popup.js
@@ -1,23 +1,6 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
-import { getDatabase, ref, set, onValue, remove, off } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-database.js";
-
-// Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDq0BESYmeLklyKaNgA7KlGGR4TyHinHuo",
-  authDomain: "yobuddy-89a58.firebaseapp.com",
-  projectId: "yobuddy-89a58",
-  storageBucket: "yobuddy-89a58.firebasestorage.app",
-  messagingSenderId: "873396866801",
-  appId: "1:873396866801:web:b84e17c8a6514496c3a350",
-  measurementId: "G-87LCDY7YN6"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const database = getDatabase(app);
-const provider = new GoogleAuthProvider();
+import { app, auth, database, provider } from './firebase-config.js';
+import { signInWithPopup, signOut } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+import { ref, set, onValue, remove, off } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-database.js";
 
 class YoBuddyApp {
   constructor() {
