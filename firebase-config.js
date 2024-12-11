6eeff9ce1e1a3464c75e25668a11188871938259
Zubeid Hendricks <zubeid.hendricks@gmail.com>
2024-12-11 19:15:55 +0200

    Update Firebase configuration with databaseURL

--- firebase-config.js
+++ firebase-config.js
@@ -2,8 +2,9 @@
 const firebaseConfig = {
   apiKey: "AIzaSyDq0BESYmeLklyKaNgA7KlGGR4TyHinHuo",
   authDomain: "yobuddy-89a58.firebaseapp.com",
+  databaseURL: "https://yobuddy-89a58-default-rtdb.firebaseio.com",
   projectId: "yobuddy-89a58",
-  storageBucket: "yobuddy-89a58.firebasestorage.app",
+  storageBucket: "yobuddy-89a58.appspot.com",
   messagingSenderId: "873396866801",
   appId: "1:873396866801:web:b84e17c8a6514496c3a350"
 };
